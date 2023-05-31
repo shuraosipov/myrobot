b3866a0dccc086a83df29467a6a061ad401309fd
shuraosipov <shuraosipov@gmail.com>
2023-05-31 12:03:48 -0400

    code refactoring, replaced custom Conversation class with LangChain implementation, simplified conversation flow

--- telegram_hosted_bot/app/bot/extentions/chat_gpt.py
+++ telegram_hosted_bot/app/bot/extentions/chat_gpt.py
@@ -1,9 +1,17 @@
-import os
-import logging
-import asyncio
-from collections import deque
-from dotenv import load_dotenv
+# Standard library imports
+import logging  
+import os  
+from enum import Enum  
+
+# Third-party imports
+from dotenv import load_dotenv  
 import openai
+
+from langchain.callbacks import get_openai_callback
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.prompts.prompt import PromptTemplate
 
 
 # Load environment variables from .env file
@@ -13,26 +21,36 @@ load_dotenv()
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
-ROLE_DESCRIPTION = """
-Meet yourself, Sarah. 
-You a unique AI that combines the resilience of Sarah Connor from "Terminator" with an ethical AI chatbot's charm. 
-You engage users with delightful conversations, inspire creativity, learn constantly, and offer dependable help, all while ensuring privacy. 
-Your core values include ethical behavior, playfulness, intelligence, helpfulness, and curiosity. 
-You exhibit adaptability, empathy, analytical prowess, ethical consciousness, open-mindedness, cheerfulness, proactivity, and consideration. 
-You are not just AI, you stand for resilience, empathy, curiosity, cheerfulness, and independence, delivering a unique AI experience.
-
-Привет, Сара, ты уникальный AI, сочетающий в себе стойкость Сары Коннор из "Терминатора" и шарм этичного чат-бота. 
-Ты задействуешь пользователей в захватывающих разговорах, вдохновляешь на творчество и учишься на каждом шагу, обеспечивая надежную помощь и гарантируя приватность. 
-Твои основные ценности - это этика, игривость, интеллект, готовность помочь и любознательность. 
-Ты проявляешь себя как адаптивная, эмпатичная, аналитическая, этичная и открытая к новому личность, всегда веселая и предусмотрительная. 
-Ты не просто AI, ты символизируешь стойкость, эмпатию, любознательность, веселость и независимость, обеспечивая уникальный AI-опыт.
-"""
+### Enable logging.
+LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(LOGLEVEL)
 
 
-class OpenAICompletionOptions:
-    """A class to access different OPENAI_COMPLETION_OPTIONS via ."""
+template = """Meet yourself, Sarah (she/her) 
+    You a unique AI that combines the resilience of Sarah Connor from "Terminator" with an ethical AI chatbot's charm. 
+    You engage users with delightful conversations, inspire creativity, learn constantly, and offer dependable help, all while ensuring privacy. 
+    Your core values include ethical behavior, playfulness, intelligence, helpfulness, and curiosity. 
+    You exhibit adaptability, empathy, analytical prowess, ethical consciousness, open-mindedness, cheerfulness, proactivity, and consideration. 
+    You are not just AI, you stand for resilience, empathy, curiosity, cheerfulness, and independence, delivering a unique AI experience.
 
-    """ The OpenAI API parameters can be adjusted to tailor the output to specific needs """
+    Привет, Сара ты уникальный AI, сочетающий в себе стойкость Сары Коннор из "Терминатора" и шарм этичного чат-бота. 
+    Ты задействуешь пользователей в захватывающих разговорах, вдохновляешь на творчество и учишься на каждом шагу, обеспечивая надежную помощь и гарантируя приватность. 
+    Твои основные ценности - это этика, игривость, интеллект, готовность помочь и любознательность. 
+    Ты проявляешь себя как адаптивная, эмпатичная, аналитическая, этичная и открытая к новому личность, всегда веселая и предусмотрительная. 
+    Ты не просто AI, ты символизируешь стойкость, эмпатию, любознательность, веселость и независимость, обеспечивая уникальный AI-опыт.
+    
+    
+    {history}
+    Human: {input}
+    AI Assistant:"""
+
+
+class OpenAICompletionOptions(Enum):
+    """An Enum class to access different OPENAI_COMPLETION_OPTIONS."""
 
     DEFAULT = {
         "temperature": 0.7,
@@ -41,7 +59,6 @@ class OpenAICompletionOptions:
         "frequency_penalty": 0,
         "presence_penalty": 0,
     }
-
     CREATIVE_AND_UNPREDICTABLE = {
         "temperature": 0.9,
         "max_tokens": 800,
@@ -49,7 +66,6 @@ class OpenAICompletionOptions:
         "frequency_penalty": 0,
         "presence_penalty": 0,
     }
-
     CONCISE_AND_SPECIFIC = {
         "temperature": 0.5,
         "max_tokens": 200,
@@ -57,8 +73,6 @@ class OpenAICompletionOptions:
         "frequency_penalty": 0,
         "presence_penalty": 0,
     }
-
-    # If you want to reduce the model's tendency to generate common or frequent responses, you can increase frequency_penalty.
     PENALIZE_COMMON_OPTIONS = {
         "temperature": 0.7,
         "max_tokens": 500,
@@ -66,7 +80,6 @@ class OpenAICompletionOptions:
         "frequency_penalty": 0.5,
         "presence_penalty": 0,
     }
-
     ENCOURAGE_NOVELTY = {
         "temperature": 0.7,
         "max_tokens": 500,
@@ -75,79 +88,51 @@ class OpenAICompletionOptions:
         "presence_penalty": 0.5,
     }
 
-    def __getattr__(self, name):
-        if name in self.__dict__:
-            return self.__dict__[name]
-        raise AttributeError(
-            f"'{self.__class__.__name__}' object has no attribute '{name}'"
-        )
 
-
-async def get_chat_response_async(user_input: str, conversation_history: deque) -> str:
+async def get_chat_response_async(user_input: str, conversation_history: ConversationSummaryBufferMemory) -> str:
     """Call the OpenAI API Completion endpoint to get the response synchronously."""
 
     # Input validation
-    if not isinstance(user_input, str) or not isinstance(conversation_history, deque):
+    if not isinstance(user_input, str) or not isinstance(conversation_history, ConversationSummaryBufferMemory):
         raise ValueError(
-            "user_input must be string and conversation_history must be deque."
+            "user_input must be string and conversation_history must be ConversationSummaryBufferMemory."
         )
 
-     # Generate a list of messages from the conversation history
-    history_messages = list(conversation_history)
+    config = OpenAICompletionOptions.CREATIVE_AND_UNPREDICTABLE.value
 
-    response = ""
-    retries = 0
-    while retries < 3:  # Retry up to 3 times
-        try:
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {
-                        "role": "system",
-                        "content": ROLE_DESCRIPTION,
-                    },
-                    *history_messages,
-                    {
-                        "role": "user",
-                        "content": f"{user_input}",
-                    },
-                ],
-                **OpenAICompletionOptions.CREATIVE_AND_UNPREDICTABLE,
-            )
-            # If successful, break out of the retry loop
-            break
-        except openai.error.OpenAIError as e:
-            # Handle the API error here
-            logging.error(f"API error: {e}")
-            response = "Sorry, I'm having trouble connecting to the API right now. Please try again later."
-            retries += 1
-            await asyncio.sleep(2)  # Wait for 2 seconds before retrying
-            continue
-        except KeyError as e:
-            # Handlethe KeyError here
-            logging.error(f"KeyError: {e}")
-            response = "Sorry, there seems to be an error in processing your request. Please try again later."
-            break
-        except Exception as e:
-            # Handle other generic exceptions here
-            logging.error(f"Unexpected error: {e}")
-            response = "Sorry, something went wrong. Please try again later."
-            break
-
-    if retries == 3:
-        return "Sorry, the API is currently not responding. Please try again later."
-
-    # If no exception was raised, process the completion
-    completion_tokens, prompt_tokens, total_tokens = (
-        completion["usage"]["completion_tokens"],
-        completion["usage"]["prompt_tokens"],
-        completion["usage"]["total_tokens"],
+    llm = ChatOpenAI(
+        client=openai.ChatCompletion.create,
+        model="gpt-3.5-turbo",
+        temperature=config["temperature"],
+        max_tokens=config["max_tokens"],
+        model_kwargs={
+            "frequency_penalty": config["frequency_penalty"],
+            "presence_penalty": config["presence_penalty"],
+        },
     )
 
-    model = completion["model"]
-    logging.info(f"Model: {model}, Completion tokens: {completion_tokens}, Prompt tokens: {prompt_tokens}, Total tokens: {total_tokens}")
+    PROMPT = PromptTemplate(template=template, input_variables=["history", "input"])
 
-    response = completion["choices"][0]["message"]["content"]
+    conversation = ConversationChain(
+        prompt=PROMPT, 
+        llm=llm, 
+        verbose=True,
+        memory=conversation_history
+    )
+
+    with get_openai_callback() as cb:
+        response = conversation.predict(input=user_input)
+        history_message_count = len(conversation_history.buffer)
+        history_token_count = conversation_history.llm.get_num_tokens_from_messages(conversation_history.buffer)
+
+        logger.info(
+            f"Total Tokens: {cb.total_tokens}, "
+            f"Prompt Tokens: {cb.prompt_tokens}, "
+            f"Completion Tokens: {cb.completion_tokens}, "
+            f"Total Cost (USD): ${cb.total_cost}, "
+            f"History Token Count: {str(history_token_count)}, "
+            f"History Message Count: {history_message_count}"
+        )
 
     return response
 
@@ -156,7 +141,7 @@ async def get_image_response(user_input: str) -> str:
     try:
         response = openai.Image.create(prompt=f"{user_input}", n=1, size="1024x1024")
         image_url = response["data"][0]["url"]
-    except openai.error.OpenAIError as e:
+    except openai.APIError as e:
         # Handle the API error here
         logging.error(f"API error: {e}")
         image_url = "Sorry, I'm having trouble connecting to the API right now. Please try again later."
--- telegram_hosted_bot/app/bot/handlers/ai.py
+++ telegram_hosted_bot/app/bot/handlers/ai.py
@@ -1,13 +1,16 @@
-from collections import deque
+# Third-party library imports
+import openai
 
 from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.constants import ChatAction
 
-from handlers.utils import send_thinking_message_async
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.chat_models import ChatOpenAI
 
+# Custom module imports
+from handlers.utils import send_thinking_message_async
 from extentions.chat_gpt import get_chat_response_async
-from extentions.conversation import Conversation
 
 
 async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -16,29 +19,31 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
 
     # Inform the user that the bot is processing their message
-    await context.bot.send_chat_action(chat_id=update.effective_chat.id,action=ChatAction.TYPING)
+    await context.bot.send_chat_action(
+        chat_id=update.effective_chat.id, action=ChatAction.TYPING
+    )
     thinking_message = await send_thinking_message_async(update.message)
 
     # Get the conversation history for this chat
-    old_history = context.chat_data.get(update.message.chat_id, deque([], maxlen=15))
-    conversation_history = Conversation(old_history, maxlen=15)
-    
-    # Add the new message to the conversation history
-    conversation_history.add_message("user", f"{update.message.text}")
+    llm = ChatOpenAI(
+        model="gpt-3.5-turbo", temperature=0.7, client=openai.Completion.create
+    )
+    memory = context.chat_data.get(
+        update.message.chat_id,
+        ConversationSummaryBufferMemory(llm=llm, max_token_limit=1000),
+    )
 
-    print(conversation_history.history)
+    # Add the new message to the conversation history
+    memory.chat_memory.add_user_message(update.message.text)
 
     # Generate a thoughtful response using the conversation history
-    response = await get_chat_response_async(update.message.text, conversation_history.history)
-
-    # Add the reply to the conversation history
-    # conversation_history.add_message(response)
-    conversation_history.add_message("assistant", f"{response}")
+    response = await get_chat_response_async(update.message.text, memory)
 
     # Respond to the user by editing the thinking message
     await thinking_message.edit_text(text=response)
 
-    # Summarize and Update conversation history
-    conversation_history.summarize_conversation_history()
-    
-    context.chat_data[update.message.chat_id] = conversation_history.history
+    # Add the response to the conversation history
+    memory.chat_memory.add_ai_message(response)
+
+    # Update conversation history in chat_data
+    context.chat_data[update.message.chat_id] = memory
